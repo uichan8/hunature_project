2149fd9f335340be5921999d31406e7dd28bb02a
uichan <uichan8@naver.com>
2022-07-22 15:46:23 +0900

    upgrade preproc func - cv2.resize -> albumentations.LongestMaxSize

--- DetectionTools/preprocess.py
+++ DetectionTools/preprocess.py
@@ -4,8 +4,9 @@ import albumentations as A
 import albumentations
 
 def preprocess(img, input_size):
-    transform_lst = [A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225],max_pixel_value=1, always_apply=True)]
-    transforms = albumentations.Compose(transform_lst)
+    #이미지 변환 도구
+    Resize = A.Compose([A.LongestMaxSize(input_size[0])])
+    Normalize = A.Compose(A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225],max_pixel_value=1, always_apply=True))
     swap = (2, 0, 1)
 
     #이미지 기틀 생성
@@ -18,14 +19,10 @@ def preprocess(img, input_size):
     r = min(input_size[0] / img.shape[0], input_size[1] / img.shape[1])
 
     #이미지 크기 조정
-    resized_img = cv2.resize(
-        img,
-        (int(img.shape[1] * r), int(img.shape[0] * r)),
-        interpolation=cv2.INTER_LINEAR,
-    ).astype(np.uint8)
+    resized_img = Resize(image=img)['image']
     padded_img[: int(img.shape[0] * r), : int(img.shape[1] * r)] = resized_img
 
     padded_img = padded_img[:, :, ::-1]
-    padded_img = transforms(image=padded_img)['image'].transpose(swap)
+    padded_img = Normalize(image=padded_img)['image'].transpose(swap)
 
     return padded_img, r
